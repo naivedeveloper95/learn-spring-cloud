32a55781779766243108e88bb4e68b26db8d012a
naivedeveloper95 <findsatyam95@gmail.com>
2023-09-19 23:17:39 +0530

    Added 2 modules

---
+++ ticket-service-spring-cloud/src/test/java/com/satyam/ticketservicespringcloud/TicketServiceSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyam.ticketservicespringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
