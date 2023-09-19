a98776047e7806d4cab5122fc490296a13ebecbd
naivedeveloper95 <findsatyam95@gmail.com>
2023-09-20 00:36:02 +0530

    Added 2 modules

--- price-service-spring-cloud/src/main/java/com/satyam/priceservicespringcloud/repository/PriceRepository.java
+++ price-service-spring-cloud/src/main/java/com/satyam/priceservicespringcloud/repository/PriceRepository.java
@@ -4,4 +4,5 @@ import com.satyam.priceservicespringcloud.model.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PriceRepository extends JpaRepository<Price, Long> {
+    Price getPriceByCode(String code);
 }
